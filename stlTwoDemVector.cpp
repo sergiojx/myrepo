3b9132988cf6e2dd233264d97323ac5141ff74cc
sergiojx <sf_acosta@hotmail.com>
2016-02-23 09:22:38 -0500

    Add stlTwoDemVector.cpp
    Multidimentional STL vector example

---
+++ stlTwoDemVector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	// Declare size of two dimensional array and initialize.
+	vector< vector<int> > vI2Matrix(3, vector<int>(2, 0));
+
+	vI2Matrix[0][0] = 0;
+	vI2Matrix[0][1] = 1;
+	vI2Matrix[1][0] = 10;
+	vI2Matrix[1][1] = 11;
+	vI2Matrix[2][0] = 20;
+	vI2Matrix[2][1] = 21;
+
+	cout << "Loop by index:" << endl;
+
+	int ii, jj;
+	for (ii = 0; ii < 3; ii++)
+	{
+		for (jj = 0; jj < 2; jj++)
+		{
+			cout << vI2Matrix[ii][jj] << endl;
+		}
+	}
+
+	return 0;
+}
